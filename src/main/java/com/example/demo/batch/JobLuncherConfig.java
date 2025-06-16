25b84ccafd373b8e09c3f287d0f6d97de36163d6
Ben Coepp <ben.coeppicus@bencoepp.de>
2025-06-16 16:07:16 +0200

    add CsvFileService dependency to JobLauncherConfig and log CSV file URLs

--- src/main/java/com/example/demo/batch/JobLuncherConfig.java
+++ src/main/java/com/example/demo/batch/JobLuncherConfig.java
@@ -1,21 +1,30 @@
 package com.example.demo.batch;
 
+import com.example.demo.service.CsvFileService;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 @Configuration
 public class JobLuncherConfig {
 
+    private final CsvFileService csvFileService;
+
+    public JobLuncherConfig(CsvFileService csvFileService) {
+        this.csvFileService = csvFileService;
+    }
+
     @Bean
     public CommandLineRunner runJob(JobLauncher jobLauncher, Job simpleJob) {
+        List<String> allCsvUrls = csvFileService.getCsvFileUrls();
+        System.out.println("CSV Urls: " + allCsvUrls.size());
         return args -> {
             JobParameters parameters = new JobParametersBuilder()
                     .addLong("time", System.currentTimeMillis()) // Unique ID
