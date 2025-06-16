0a7945aee53088c859e2c1c6934f8ceff6e91d63
Ben Coepp <ben.coeppicus@bencoepp.de>
2025-06-16 13:11:12 +0200

    enhance Measurement and Station entities with additional fields and Lombok annotations

--- src/main/java/com/example/demo/entity/Measurement.java
+++ src/main/java/com/example/demo/entity/Measurement.java
@@ -1,14 +1,29 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Measurement {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    public String id;
+    private String id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Station station;
+    private Long precipitation;
+    private Long snowfall;
+    private Long snowDepth;
+    private Long maxTemperature;
+    private Long minTemperature;
+    private LocalDateTime peakWindGust;
+    private Long peakWindGustSpeed;
+    private String peakWindGustDirection;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 }
--- src/main/java/com/example/demo/entity/Station.java
+++ src/main/java/com/example/demo/entity/Station.java
@@ -4,11 +4,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Station {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    public String id;
+    private String id;
+    private String stationId;
+    private String name;
+    private Long latitude;
+    private Long longitude;
+    private Long elevation;
+    private LocalDateTime created;
+    private LocalDateTime updated;
 }
